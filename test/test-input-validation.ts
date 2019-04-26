571d37e8d88d4a27e263f4c5bbaf887f209364cb
Eric Crosson <eric.s.crosson@utexas.edu>
2019-04-25 20:36:31 -0500

    Comment tests covered by static types

--- test/test-input-validation.ts
+++ test/test-input-validation.ts
@@ -7,13 +7,16 @@ import test from 'ava'
 import nextSessionGenerator from '../src/next-session-generator'
 
 
-test('should error when timeframe is not a string', t => {
-    const nextSession = nextSessionGenerator(4, new Date())
-    const error = t.throws(() => {
-        nextSession.next()
-    })
-    t.is(error.name, 'ArgumentError')
-})
+/**
+ * Note: has to be commented thanks to strong-types
+ */
+// test('should error when timeframe is not a string', t => {
+//     const nextSession = nextSessionGenerator(4, new Date())
+//     const error = t.throws(() => {
+//         nextSession.next()
+//     })
+//     t.is(error.name, 'ArgumentError')
+// })
 
 test('should error when timeframe is not in trading-view format', t => {
     const nextSession = nextSessionGenerator('!!', new Date())
@@ -23,10 +26,13 @@ test('should error when timeframe is not in trading-view format', t => {
     t.is(error.name, 'ArgumentError')
 })
 
-test('should error when start is not a date', t => {
-    const nextSession = nextSessionGenerator('1D', 'horse')
-    const error = t.throws(() => {
-        nextSession.next()
-    })
-    t.is(error.name, 'ArgumentError')
-})
+/**
+ * Note: has to be commented thanks to strong-types
+ */
+// test('should error when start is not a date', t => {
+//     const nextSession = nextSessionGenerator('1D', 'horse')
+//     const error = t.throws(() => {
+//         nextSession.next()
+//     })
+//     t.is(error.name, 'ArgumentError')
+// })
